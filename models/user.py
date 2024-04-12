9e0e0e3bab59bf572080f3565023d08719bc2ecf
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-12 16:52:34 -0500

    Changes nullable the column first_name and last_name

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), default="NULL", nullable=False)
-    last_name = Column(String(128), default="NULL", nullable=False)
+    first_name = Column(String(128), default="NULL", nullable=True)
+    last_name = Column(String(128), default="NULL", nullable=True)
     # places = relationship('Place', backref="user", cascade="delete")
     # reviews = relationship('Review', backref="user", cascade="delete")
