bb1694a9c132c41b7db2da5fcb32b9b5c04e0f2c
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-12 16:25:48 -0500

    test for the task 7

--- models/state.py
+++ models/state.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
-
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref="state", cascade="delete")
+    cities = relationship("City", backref="state", cascade="delete")
 
-    @property
-    def method_cities(self):
-        cities_for_state = []
-        for city in self.cities:
-            if city.state_id == self.id:
-                cities_for_state.append(city)
-        return cities_for_state
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def method_cities(self):
+            print(self)
+            from models.__init__ import storage
+            from models.city import City
+            cities_for_state = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_for_state.append(city)
+            return cities_for_state
