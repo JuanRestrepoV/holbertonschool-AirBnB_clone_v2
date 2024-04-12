3836708a373c1cfc578b3067cf0de5d1bf016e64
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-12 17:15:17 -0500

    Test for task numer 8

--- models/place.py
+++ models/place.py
@@ -9,16 +9,16 @@ from sqlalchemy import ForeignKey
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'))
+    user_id = Column(String(60), ForeignKey('users.id'))
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), default="NULL", nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float(0), nullable=False)
-    longitude = Column(Float(0), nullable=False)
+    latitude = Column(Float(0), nullable=True)
+    longitude = Column(Float(0), nullable=True)
     amenity_ids = []
 
     reviews = relationship('Review', backref="place", cascade="delete")
