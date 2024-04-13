e741444b2435ec460105cc25be67602937b7b1b9
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-13 01:04:56 -0500

    Test for method add

--- models/place.py
+++ models/place.py
@@ -45,8 +45,11 @@ class Place(BaseModel, Base):
         def amenities(self):
             from models.amenity import Amenity
             from models.__init__ import storage
-            return [amenity for amenity in list(storage.all(Amenity).values())
-                    if amenity.id in self.amenity_ids]
+            amenity_list = []
+            for amenity in list(storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
 
         @amenities.setter
         def amenities(self, amenity):
