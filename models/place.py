f3a54d8a8196d2bec2d4a7613c726b8afd6e0aeb
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-13 01:51:48 -0500

    Test for method add

--- models/place.py
+++ models/place.py
@@ -42,8 +42,9 @@ class Place(BaseModel, Base):
         @property
         def amenities(self):
             from models.amenity import Amenity
+            from models.engine.file_storage import FileStorage
             from models.__init__ import storage
-            return [amenity for amenity in storage.all(Amenity).values()
+            return [amenity for amenity in FileStorage.all(Amenity).values()
                     if amenity.id in self.amenity_ids]
 
         @amenities.setter
