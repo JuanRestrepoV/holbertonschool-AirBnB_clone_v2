d3caaef9520ce11225fb8dd60d601b1ffb7e6326
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-16 10:57:19 -0500

    Hot fix

--- models/place.py
+++ models/place.py
@@ -47,7 +47,7 @@ class Place(BaseModel, Base):
             return [amenity for amenity in storage.all(Amenity).values()
                     if amenity.id in self.amenity_ids]
 
-        @amenities.setter
+        @amenity_list.setter
         def amenity_list(self, amenity):
             from models.amenity import Amenity
             if isinstance(amenity, Amenity):
