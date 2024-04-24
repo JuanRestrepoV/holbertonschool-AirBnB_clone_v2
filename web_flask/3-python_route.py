91e5250a388b72c4867b8277d15d7cdfa28f0b76
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-04-24 17:54:47 -0500

    test tak 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -38,7 +38,7 @@ def cisfun(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route("/python/", defaults={"text": "is_cool"})
+@app.route('/python/', defaults={"text": "is_cool"})
 @app.route("/python/<text>", strict_slashes=False)
 def pythoniscool(text):
     """Root route that returns '/python/<text>'
